242af34d316ded363ee7e01ef96141be18415122
islam <isl13mouhamed@gmail.com>
2026-02-09 23:49:12 +0100

    Server: Increase payload limit to 50mb to support Rich Text images

--- server/index.js
+++ server/index.js
@@ -17,7 +17,8 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(morgan('dev'));
 app.use(helmet({
     crossOriginResourcePolicy: false, // Allow loading images from different origins/same site
